9045a579a048989f57261b79a3b5cf3437cb4d86
Ítalo H. <italodssouza.backup@gmail.com>
2025-12-08 16:57:16 -0300

    Creating libraryapi project

---
+++ libraryapi/src/test/java/tech/kvothe/libraryapi/LibraryapiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kvothe.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
