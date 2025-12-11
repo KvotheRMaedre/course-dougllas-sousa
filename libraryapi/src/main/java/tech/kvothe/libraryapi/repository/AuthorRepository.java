d9123c7c225a9a0fa9bf3a7dc788d3ed54e264d6
Ítalo H. <italodssouza.backup@gmail.com>
2025-12-11 09:50:38 -0300

    libraryapi: renaming repositories

--- libraryapi/src/main/java/tech/kvothe/libraryapi/repository/AuthorRepository.java
+++ libraryapi/src/main/java/tech/kvothe/libraryapi/repository/AuthorRepository.java
@@ -1,8 +1,9 @@
 package tech.kvothe.libraryapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import tech.kvothe.libraryapi.model.Author;
 
 import java.util.UUID;
 
-public interface Book extends JpaRepository<Book, UUID> {
+public interface AuthorRepository extends JpaRepository<Author, UUID> {
 }
--- libraryapi/src/main/java/tech/kvothe/libraryapi/repository/BookRepository.java
+++ libraryapi/src/main/java/tech/kvothe/libraryapi/repository/BookRepository.java
@@ -1,8 +1,9 @@
 package tech.kvothe.libraryapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import tech.kvothe.libraryapi.model.Book;
 
 import java.util.UUID;
 
-public interface Author extends JpaRepository<Author, UUID> {
+public interface BookRepository extends JpaRepository<Book, UUID> {
 }
