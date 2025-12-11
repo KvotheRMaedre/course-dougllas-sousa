3840ca4aae929636aa4b6b176bf1fa375c68333f
Ítalo H. <italodssouza.backup@gmail.com>
2025-12-11 09:51:10 -0300

    libraryapi: adding new properties

--- libraryapi/src/main/java/tech/kvothe/libraryapi/model/Author.java
+++ libraryapi/src/main/java/tech/kvothe/libraryapi/model/Author.java
@@ -1,17 +1,18 @@
 package tech.kvothe.libraryapi.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "author")
-@Getter
-@Setter
+@Table(name = "author", schema = "public")
+@ToString(exclude = {"books"})
 public class Author {
 
     @Id
@@ -27,6 +28,17 @@ public class Author {
     @Column(name = "nationality", nullable = false, length = 50)
     private String nationality;
 
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "id_user")
+    private UUID idUser;
+
     @OneToMany(mappedBy = "author")
     private List<Book> books;
 
@@ -37,5 +49,69 @@ public class Author {
         this.name = name;
         this.birthday = birthday;
         this.nationality = nationality;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(UUID idUser) {
+        this.idUser = idUser;
     }
 }
--- libraryapi/src/main/java/tech/kvothe/libraryapi/model/Book.java
+++ libraryapi/src/main/java/tech/kvothe/libraryapi/model/Book.java
@@ -2,13 +2,18 @@ package tech.kvothe.libraryapi.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
 @Table(name = "book")
-@Data
+@ToString(exclude = {"author"})
 public class Book {
 
     @Id
@@ -29,7 +34,18 @@ public class Book {
     private BookGenre genre;
 
     @Column(name = "price", precision = 18, scale = 2)
-    private Double price;
+    private BigDecimal price;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "id_user")
+    private UUID idUser;
 
     @ManyToOne
     @JoinColumn(name = "id_author")
@@ -38,5 +54,92 @@ public class Book {
     public Book() {
     }
 
+    public Book(String isbn, String title, LocalDate publicationDate, BookGenre genre, BigDecimal price, Author author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.publicationDate = publicationDate;
+        this.genre = genre;
+        this.price = price;
+        this.author = author;
+    }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public BookGenre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(BookGenre genre) {
+        this.genre = genre;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(UUID idUser) {
+        this.idUser = idUser;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
