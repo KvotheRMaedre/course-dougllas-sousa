8a11a2e888dd511b7155827e2f31a92550f8b468
Ítalo H. <italodssouza.backup@gmail.com>
2025-12-15 11:38:44 -0300

    libraryapi: adding a generic exception for unhandled exceptions

--- libraryapi/src/main/java/tech/kvothe/libraryapi/controller/ExceptionHandlerController.java
+++ libraryapi/src/main/java/tech/kvothe/libraryapi/controller/ExceptionHandlerController.java
@@ -11,8 +11,16 @@ import tech.kvothe.libraryapi.dto.ParamErrorDTO;
 import tech.kvothe.libraryapi.exception.DuplicatedResourceException;
 import tech.kvothe.libraryapi.exception.OperationNotAllowedException;
 
+import java.util.List;
+
 @RestControllerAdvice
 public class ExceptionHandlerController {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponseDTO handleUnhandledExceptions(RuntimeException exception) {
+        return new ErrorResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "", List.of());
+    }
 
     @ExceptionHandler(DuplicatedResourceException.class)
     public ResponseEntity<ErrorResponseDTO>handleDuplicateResourceException(DuplicatedResourceException exception) {
