6629d84e0056108e8c650ebf43c40f93b4a6546e
Ítalo H. <italodssouza.backup@gmail.com>
2025-12-17 15:29:31 -0300

    libraryapi: adding custom data into the jwt token

--- libraryapi/src/main/java/tech/kvothe/libraryapi/config/AuthorizationServerConfiguration.java
+++ libraryapi/src/main/java/tech/kvothe/libraryapi/config/AuthorizationServerConfiguration.java
@@ -11,21 +11,27 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import tech.kvothe.libraryapi.security.CustomAuthentication;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -95,5 +101,23 @@ public class AuthorizationServerConfiguration {
     @Bean
     public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
         return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> auth2TokenCustomizer() {
+        return context -> {
+            var principal = context.getPrincipal();
+            if (principal instanceof CustomAuthentication customAuthentication) {
+                OAuth2TokenType tokenType = context.getTokenType();
+
+                if (OAuth2TokenType.ACCESS_TOKEN.equals(tokenType)) {
+                    var authorities = customAuthentication.getAuthorities();
+                    List<String> autoritiesList = authorities.stream().map(GrantedAuthority::getAuthority).toList();
+                    context.getClaims()
+                            .claim("authorities", autoritiesList)
+                            .claim("email", customAuthentication.getUser().getEmail());
+                }
+            }
+        };
     }
 }
