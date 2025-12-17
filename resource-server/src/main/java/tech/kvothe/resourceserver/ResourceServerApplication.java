e465fc77fe3e585a53e7e62667515e6fc6fdb02a
Ítalo H. <italodssouza.backup@gmail.com>
2025-12-17 15:38:29 -0300

    resource-server: creating project

---
+++ resource-server/src/main/java/tech/kvothe/resourceserver/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package tech.kvothe.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerApplication.class, args);
+	}
+
+}
